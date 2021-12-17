e7dcda658936befd22a1d7f2f272a0cb349a07f0
Dusan Vukadinovic <vukadinovic@mps.mpg.de>
2021-12-17 17:47:03 +0100

    added points for 'vispec' software

--- vispec/main.py
+++ vispec/main.py
@@ -1,3 +1,23 @@
 import tkinter as tk
 from astropy.io import fits
 
+"""
+At start we have small window with TextBox's and Button.
+Additionally, we initialize the window for plotting to which
+we draw everything.
+
+This figure will be cleared before we replot something else.
+
+Field for path to observed spectrum.
+Field for path to inverted spectrum (optional).
+Field for path to atmosphere.
+Field for path to inverted atmosphere (optional).
+
+Plot options:
+	-- which Stokes components we plot (single or all)
+	-- which atmospheric parameter we plot (one or more)
+
+Field for picking the pixel position to plot.
+
+Button to generate the spectrum in separate window.
+"""
