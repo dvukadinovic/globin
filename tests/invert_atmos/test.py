3fa90e7c045808b761452cb43a1c7a5c6132397b
dusan.vukadinovic01 <vukadinovic@mps.mpg.de>
2022-07-03 22:54:04 +0200

    added files from testing the code

---
+++ tests/invert_atmos/test.py
@@ -0,0 +1,25 @@
+import globin
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+# cont = globin.Globin()
+# cont.read_input(run_name="test")
+
+inverter = globin.Inverter()
+inverter.read_input(run_name="pryh_test")
+spec = inverter.run()
+
+sys.exit()
+
+# import timeit
+# def fun():
+# 	cont.compute_spectra(parallel=True)
+# times = timeit.Timer(fun).repeat(repeat=3, number=10)
+# print(times)
+
+obs = globin.Observation("obs_globin.fits")
+
+plt.plot(spec.spec[0,2,:,3] - obs.spec[0,2,:,3])
+# plt.plot(obs.spec[0,2,:,3])
+plt.show()
