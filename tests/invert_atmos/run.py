76f684a6f2cf5e96a3a608ebac2fdecd8bb9176a
dvukadinovic <vukadinovic.dusan.94@gmail.com>
2022-09-04 23:21:34 +0200

    updated tests for pixel-by-pixel inversion after improving the speed of mode=1 and 2

--- tests/invert_atmos/run.py
+++ tests/invert_atmos/run.py
@@ -8,7 +8,7 @@ import globin
 obs = globin.Observation("obs.fits")
 atmos = globin.Atmosphere("atmos_HSE_pyrh.fits")
 
-inv = globin.Atmosphere("runs/dummy/inverted_atmos.fits")
+# inv = globin.Atmosphere("runs/dummy/inverted_atmos.fits")
 
 # params = ["temp", "vz", "vmic", "mag", "gamma", "chi"]
 # params = ["ne", "nH"]
@@ -20,10 +20,28 @@ inv = globin.Atmosphere("runs/dummy/inverted_atmos.fits")
 
 inverter = globin.Inverter()
 inverter.read_input(run_name="dummy")
-atmos, inv_spec = inverter.run()
+inv_atmos, inv_spec = inverter.run()
 
-globin.visualize.plot_spectra(obs.spec[0,0], obs.wavelength, inv=inv_spec.spec[0,0])
-plt.show()
+# inv_atmos.compare(atmos.data[0,0], idx=0, idy=0)
+# inv_atmos.compare(atmos.data[0,1], idx=0, idy=1)
+# inv_atmos.compare(atmos.data[0,2], idx=0, idy=2)
+
+# plt.plot(inv_atmos.data[0,0,2])
+
+# plt.plot(inv_atmos.data[0,1,2])
+# # plt.plot(inv_atmos.data[0,1,2])
+# # plt.show()
+
+# plt.plot(inv_atmos.data[0,2,2])
+# # plt.plot(inv_atmos.data[0,2,2])
+
+# plt.yscale("log")
+# plt.show()
+
+for idx in range(inv_spec.nx):
+	for idy in range(inv_spec.ny):
+		globin.visualize.plot_spectra(inverter.observation.spec[idx,idy], inverter.observation.wavelength, inv=inv_spec.spec[idx,idy])
+		plt.show()
 
 sys.exit()
 
