f5981db10945f55872c4835a4f57f1e2d2350bf3
Dusan Vukadinovic <vukadinovic@mps.mpg.de>
2022-11-25 12:48:40 +0100

    added AMU as a constant

--- globin/__init__.py
+++ globin/__init__.py
@@ -136,6 +136,10 @@ from scipy.constants import c as LIGHT_SPEED
 from scipy.constants import h as PLANCK
 from scipy.constants import m_e as ELECTRON_MASS
 from scipy.constants import e as ELECTRON_CHARGE
+from scipy.constants import physical_constants as CONSTANTS
+
+AMU = CONSTANTS["atomic mass constant"][0]
+
 from scipy.interpolate import splrep
 
 #--- FAL C model (ref.): reference model if not given otherwise
