fixed the bug in check if v LOS has changed the sign during extrapolation

globin/atmos.py
@@ -758,9 +758,9 @@ class Atmosphere(object):
 					ind = np.argmin(np.abs(self.logtau-x[-1]))
 					for idz in range(ind, self.nz):
 						if y[-1]>0:
-							y_new[idz] = np.min([y_new[idz], y[-1]/2])
-						if y[-1]<0:
 							y_new[idz] = np.max([y_new[idz], y[-1]/2])
+						if y[-1]<0:
+							y_new[idz] = np.min([y_new[idz], y[-1]/2])
 
 			atmos.data[idx,idy,atmos.par_id[parameter],:] = y_new
 
@@ -1547,7 +1547,6 @@ class Atmosphere(object):
 			values = self.values[parameter]
 			perturbation = self.delta[parameter]
 
-			# for nodeID in trange(len(nodes), desc=f"{parameter} nodes", leave=None, ncols=0):
 			for nodeID in range(len(nodes)):
 				if self.mask[parameter][nodeID]==0:
 					continue			

globin/input.py
@@ -334,7 +334,7 @@ class InputData(object):
 
 		#--- resample and normalize the instrumental profile to specified wavelength grid
 		if self.instrumental_profile is not None:
-			fun = interp1d(self.instrumental_wave, self.instrumental_profile, fill_value=0)
+			fun = interp1d(self.instrumental_wave/10, self.instrumental_profile, fill_value=0)
 			dlam = self.step*10
 			N = (self.instrumental_wave.max() - self.instrumental_wave.min()) / (dlam)
 			M = np.ceil(N)//2
@@ -342,6 +342,7 @@ class InputData(object):
 			xnew = np.linspace(0, (M-1)*dlam, num=M)
 			aux = np.linspace(-(M-1)*dlam, -dlam, num=M-1)
 			xnew = np.append(aux, xnew)
+			xnew /= 10 # [A --> nm] beacause we make observations in nm scale
 			aux = fun(xnew)
 			self.instrumental_profile = aux/np.sum(aux)
 