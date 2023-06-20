9b4ee6f91d3fe9fb50ed48aa05143b88356bf880
Dusan Vukadinovic <vukadinovic@mps.mpg.de>
2023-06-20 14:48:47 +0200

    bug fix in mode=3 for intermediate results output

--- globin/inversion.py
+++ globin/inversion.py
@@ -869,7 +869,7 @@ class Inverter(InputData):
 				break_flag = True
 
 			#--- save intermediate results every 'output_frequency' iteration
-			if self.output_frequency!=self.max_iter:
+			if self.output_frequency!=max_iter:
 				if itter%self.output_frequency==0 and itter!=0:
 					self.save_cycle(chi2, obs, corrected_spec, atmos, 0)
 
