a8e12766452c844641c568a8a7a7005b1a525e9c
Dusan Vukadinovic <vukadinovic@mps.mpg.de>
2025-02-12 12:41:32 +0100

    updated imshow plots to handle arbitrary number of second atmospheres and updated the figure style

--- vispec/tools.py
+++ vispec/tools.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as mpl
 
 WIDTH = 3
 HEIGHT = 2 + 2/3
@@ -38,3 +39,20 @@ def create_fig(nrows=1, ncols=1, figsize=None, aspect="auto", hspace=0.3, wspace
     axs = axs.reshape(nrows, ncols)
 
     return fig, axs
+
+class MidpointNormalizeFair(mpl.colors.Normalize):
+    """ From: https://matplotlib.org/users/colormapnorms.html"""
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        mpl.colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+
+        result, is_scalar = self.process_value(value)
+        self.autoscale_None(result)
+
+        vlargest = max( abs( self.vmax - self.midpoint ), abs( self.vmin - self.midpoint ) )
+        x, y = [ self.midpoint - vlargest, self.midpoint, self.midpoint + vlargest], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
