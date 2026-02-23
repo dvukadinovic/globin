c267dbb0bce6b5144306ee273dcf7ee2a23bf04c
Dusan Vukadinovic <vukadinovic.dusan.94@gmail.com>
2026-02-23 09:10:47 +0100

    updated the example for the inversion test (suggestion: do not touch initial parameters, they are fragile :))

--- examples/invert_atmos/run.py
+++ examples/invert_atmos/run.py
@@ -5,73 +5,21 @@ import sys
 import copy
 import globin
 
-# atmos = globin.falc
-
-# atmos.load_populations("populations_Fe52_FALC.fits")
-# # atmos.load_populations("populations_FeEdvarda_FALC.fits")
-
-# nI = np.sum(atmos.n_pops["FE"][0,0,:-2], axis=0)
-# nII = atmos.n_pops["FE"][0,0,-2]
-# nIII = atmos.n_pops["FE"][0,0,-1]
-# n = nI + nII + nIII
-
-# dc = atmos.n_pops["FE"][0,0]/atmos.nstar_pops["FE"][0,0]
-
-# x = atmos.height[0,0]/1e3
-
-# # plt.plot(x, dc[0])
-# # plt.plot(x, dc[22])
-
-# # plt.plot(x, dc[8], c="C0", label="6015.18")
-# # plt.plot(x, dc[27], c="C0", ls="--")
-
-# # plt.plot(x, dc[2], c="C0", label="5247.06")
-# # plt.plot(x, dc[11], c="C0", ls="--")
-
-# plt.plot(x, dc[4], c="C1", label="5250.11")
-# plt.plot(x, dc[13], c="C1", ls="--")
-
-# plt.plot(x, dc[7], c="C2", label="5250.64")
-# plt.plot(x, dc[29], c="C2", ls="--")
-
-# plt.ylim([1e-1, 5e2])
-# plt.yscale("log")
-
-# # plt.plot(x, nI/n*100)
-
-# nI = np.sum(atmos.nstar_pops["FE"][0,0,:-2], axis=0)
-# nII = atmos.nstar_pops["FE"][0,0,-2]
-# nIII = atmos.nstar_pops["FE"][0,0,-1]
-# n = nI + nII + nIII
-
-# # plt.plot(x, nI/n*100)
-
-# ax = plt.gca().twinx()
-# ax.plot(x, atmos.T[0,0], c="k")
-# ax.set_ylabel("Temperature [K]")
-
-# plt.xlabel("Height [Mm]")
-# plt.ylabel("Departure coefficients")
-# # plt.xlim([x.min(), 1.5])
-# # plt.gca().invert_xaxis()
-
-# plt.legend(frameon=True)
-
-# plt.show()
-
-# sys.exit()
-
-inverter = globin.Inverter(verbose=True)
+inverter = globin.Inverter(verbose=False)
 inverter.read_input(run_name="dummy")
+
+inverter.atmosphere.atomic_number = np.array([26], dtype=np.int32)
+inverter.atmosphere.atomic_abundance = np.array([7.44], dtype=np.float64)
+
 if inverter.mode==0:
-    atmos = inverter.atmosphere
-    atmos.get_populations = False
+    # atmos = inverter.atmosphere
+    # atmos.get_populations = False
 
     spec = inverter.run()
 
     # obs = globin.Observation("obs_630_mu1_abs_LTE_20240918.fits")
 
-    print(atmos.n_pops)
+    # print(atmos.n_pops)
 
     # atmos.save_populations("populations_FeEdvarda_FALC.fits")
 
@@ -95,14 +43,31 @@ if inverter.mode==0:
     sys.exit()
 else:
     inv_atmos, inv, chi2 = inverter.run()
+    print(inv_atmos.global_pars["loggf"][0,0])
+    print(chi2.get_final_chi2()[0])
+    print(inv_atmos.values)
 
-idx, idy = 0,0
+sys.exit()
+
+print(inv_atmos.global_pars["loggf"][0,0])
+
+idx, idy = 1, 0
+
+for parameter in inverter.atmosphere.nodes:
+    print(inverter.reference_atmosphere.values[parameter])
+    print(inv_atmos.values[parameter])
+
 # inv = globin.Observation("runs/dummy/inverted_spectra_c1.fits")
-globin.visualize.plot_spectra(inverter.observation.spec[idx,idy], inverter.observation.wavelength, 
-    inv=[inv.spec[idx,idy]], 
-    labels=["obs", "inv"])
+globin.visualize.plot_spectra(inverter.observation.spec[idx,idy], 
+                              inverter.observation.wavelength, 
+                              inv=[inv.spec[idx,idy]], 
+                              labels=["obs", "inv"])
 
-atmos = globin.Atmosphere("atmos_bezier.fits", atm_range=[0,1,0,1])
 # inv_atmos = globin.Atmosphere("runs/dummy/inverted_atmos_c1.fits")
-globin.visualize.plot_atmosphere(inv_atmos, parameters=["temp", "vz", "mag", "vmic", "gamma", "chi"], reference=atmos)
+globin.visualize.plot_atmosphere(inv_atmos, 
+                                 parameters=["temp", "vz", "mag", "vmic", "gamma", "chi"], 
+                                 idx=idx, 
+                                 idy=idy, 
+                                 reference=inverter.reference_atmosphere)
+
 globin.show()
